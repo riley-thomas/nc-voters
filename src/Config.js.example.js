74be525c986ce9e2aeaa2864e93fe3c84ef18482
riley-thomas <jrileythomas@gmail.com>
2019-07-12 13:05:37 -0400

    update example config

--- src/Config.js.example.js
+++ src/Config.js.example.js
@@ -1,6 +1,7 @@
 
 const Config = {
 	app_title : 'Joshua\'s Voter Roll',
+	app_home : '/voters/1',
 	solr_url_voters : '<URL to select voter core>',
 	solr_url_history : '<URL to select voter history core>',
 	google_map_key : '<Your map key here>'
